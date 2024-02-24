8b2dd1886d5d21d977b5c775f5485a9ad68f3e7d
Smill3 <minecraftplay76@gmail.com>
2024-02-24 01:01:56 -0300

    app 100% fr fr this time

--- ParfumApp /ParfumApp.swiftpm/ScenariosView.swift
+++ ParfumApp /ParfumApp.swiftpm/ScenariosView.swift
@@ -12,7 +12,7 @@ struct ScenariosView: View {
                     .ignoresSafeArea()
                 
                 VStack {
-                    Image("caixaCenariosNew1")
+                    Image("caixaCenariosNew2")
                         .resizable()
                         .scaledToFit()
                         .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.9)
